2e531ce7bfce6c2074ecf622247407f1e296461a
hankmo <hankmo.x@gmail.com>
2026-04-29 20:39:48 +0800

    chore: Update build version from 'dev' to 'release' in AppVersion.swift to reflect the current release state of ClearViewApp.

--- Sources/ClearViewApp/AppVersion.swift
+++ Sources/ClearViewApp/AppVersion.swift
@@ -4,7 +4,7 @@ enum AppVersion {
     // 开源版本信息统一由这里维护，避免 README、关于窗、发布说明出现多处不一致。
     static let productName = "ClearView"
     static let version = "0.1.0"
-    static let build = "dev"
+    static let build = "release"
     static let minimumSystemVersion = "macOS 13+"
     static let license = "Apache-2.0"
 
